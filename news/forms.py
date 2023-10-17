3ff89c329cc8fe7955c15a997f2ae071c2835f41
boybest <125255781+muroshkin88@users.noreply.github.com>
2023-10-17 16:54:37 +0300

    Delete news directory

--- news/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm, ModelMultipleChoiceField
-from .models import Post, Author, Category
-
-class PostForm(ModelForm):
-    #category = ModelMultipleChoiceField(label="категория", queryset=Category.objects.all())
-    class Meta:
-        model = Post
-        fields = ['author', 'title', 'text', 'category' ]
